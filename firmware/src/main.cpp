83c0074c0a0efc4f140479d88dcb0d7149d4bb58
Nikolay Miroshnichenko <nickol@me.com>
2026-03-28 11:05:44 +0100

    Initial project structure: web flash, firmware config, docs

---
+++ firmware/src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("IN-9 Spectrum Analyzer starting...");
+}
+
+void loop() {
+  // TODO: implement spectrum analyzer logic
+}
